779f51560129572b0027a15f5364ab6ec54bbbf1
crowdbotics-dev <engineering@crowdbotics.com>
2023-03-04 14:26:50 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Addressscyihmgtch
+admin.site.register(Addressscyihmgtch)
 
 # Register your models here.
--- backend/home/api/v1/serializers.py
+++ backend/home/api/v1/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from home.models import Addressscyihmgtch
 from django.http import HttpRequest
 from django.utils.translation import ugettext_lazy as _
 from allauth.account import app_settings as allauth_settings
@@ -74,3 +75,9 @@ class UserSerializer(serializers.ModelSerializer):
 class PasswordSerializer(PasswordResetSerializer):
     """Custom serializer for rest_auth to solve reset password error"""
     password_reset_form_class = ResetPasswordForm
+
+class AddressscyihmgtchSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Addressscyihmgtch
+        fields = "__all__"
